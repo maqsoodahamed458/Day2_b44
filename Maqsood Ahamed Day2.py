3a8ad83baecaed5d2c614a42b53841ba4c3f6f57
maqsoodahamed458 <83702598+maqsoodahamed458@users.noreply.github.com>
2021-05-06 17:01:42 +0530

    Add files via upload

---
+++ Maqsood Ahamed Day2.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hello world')
+
+
+# In[3]:
+
+
+variables x,y
+
+
+# In[4]:
+
+
+x=3
+y=10
+
+
+# In[5]:
+
+
+x+y
+
+
+# In[6]:
+
+
+school='alhabeeb'
+
+
+# In[7]:
+
+
+print(school)
+
+
+# In[8]:
+
+
+2school='alhabeeb'
+
+
+# In[15]:
+
+
+school@='alhabeeb'
+
+
+# In[10]:
+
+
+name1='maqsood'
+print(name1)
+
+
+# In[11]:
+
+
+name2="ahmed"
+print(name2)
+
+
+# In[12]:
+
+
+name3="""kaneez fatima"""
+print(name3)
+
+
+# In[13]:
+
+
+kaneezfatima=786
+print(kaneezfatima)
+
+
+# In[14]:
+
+
+type(name1)
+
+
+# In[16]:
+
+
+type(name2)
+
+
+# In[17]:
+
+
+type(name3)
+
+
+# In[18]:
+
+
+type(kaneezfatima)
+
+
+# In[ ]:
+
+
+
+
